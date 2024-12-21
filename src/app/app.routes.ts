64049f24933420aac659d19c2b24d0f17e783a39
sergio <pugliese.sergio1987@hotmail.it>
2024-12-21 15:56:31 +0100

    init new project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'calendar',
-    pathMatch: 'full'
-  },
-  {
-    path: 'calendar',
-    loadComponent: () => import('./calendar-wrapper/calendar-wrapper.component').then(c => c.CalendarWrapperComponent)
-  }
-];
+export const routes: Routes = [];
